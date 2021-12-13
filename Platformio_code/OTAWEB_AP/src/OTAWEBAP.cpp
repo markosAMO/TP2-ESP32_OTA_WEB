1bf14adecba85c98bb30563b3e522f2a0647f2a7
markosAMO <marmoscoso1@gmail.com>
2021-12-13 14:08:09 -0300

    HttpUpdate
    Se trata de realizar la carga de ota por emdio de la librería httpupdate

--- Platformio_code/OTAWEB_AP/src/OTAWEBAP.cpp
+++ Platformio_code/OTAWEB_AP/src/OTAWEBAP.cpp
@@ -4,6 +4,8 @@
 #include <Update.h>
 #include <ArduinoOTA.h>
 #include "QueryLib.h"
+#include <HTTPClient.h>
+#include <HTTPUpdate.h>
 const char* ssid = "ESP32_AP";
 const char* password = "123456789";
 const char* www_username = "admin";
@@ -15,7 +17,55 @@ String variable;
  */
  
  WebServer server(80);
+void update_started() {
+  Serial.println("CALLBACK:  HTTP update process started");
+}
 
+void update_finished() {
+  Serial.println("CALLBACK:  HTTP update process finished");
+}
+
+void update_progress(int cur, int total) {
+  Serial.printf("CALLBACK:  HTTP update process at %d of %d bytes...\n", cur, total);
+}
+
+void update_error(int err) {
+  Serial.printf("CALLBACK:  HTTP update fatal error code %d\n", err);
+}
+void ota(){
+  WiFiClient client;
+
+    // The line below is optional. It can be used to blink the LED on the board during flashing
+    // The LED will be on during download of one buffer of data from the network. The LED will
+    // be off during writing that buffer to flash
+    // On a good connection the LED should flash regularly. On a bad connection the LED will be
+    // on much longer than it will be off. Other pins than LED_BUILTIN may be used. The second
+    // value is used to put the LED on. If the LED is on with HIGH, that value should be passed
+    // httpUpdate.setLedPin(LED_BUILTIN, LOW);
+
+    httpUpdate.onStart(update_started);
+    httpUpdate.onEnd(update_finished);
+    httpUpdate.onProgress(update_progress);
+    httpUpdate.onError(update_error);
+
+    t_httpUpdate_return ret = httpUpdate.update(client, "http://192.168.4.2:5000/display/firmware.bin");
+    // Or:
+    //t_httpUpdate_return ret = httpUpdate.update(client, "server", 80, "/file.bin");
+
+    switch (ret) {
+      case HTTP_UPDATE_FAILED:
+        Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
+        break;
+
+      case HTTP_UPDATE_NO_UPDATES:
+        Serial.println("HTTP_UPDATE_NO_UPDATES");
+        break;
+
+      case HTTP_UPDATE_OK:
+        Serial.println("HTTP_UPDATE_OK");
+        break;
+    }
+}
  /*
  * Para generar codigo jQuery
  */
@@ -36,14 +86,14 @@ void SetupServer() {
 
   /*
    * Manejo del endpoint '/update' para subir el archivo
-   */
-
-  server.on("/update", HTTP_POST, []() {
+  */
+  server.on("/update", HTTP_GET, []() {
+      ota();
       //server.sendHeader("Connection", "close");
       //server.send(200, "text/html", (Update.hasError()) ? "FAIL" : "OK");
-      Serial.println("conecction successful");
-      Serial.println(server.argName(0));
-      String var = server.arg(("plain"));
+      //Serial.println("conecction successful");
+      //Serial.println(server.argName(0));
+      //String var = server.arg(("plain"));
       Serial.println();
       server.send(200, "text/plain", String("POST realizado"));
 
